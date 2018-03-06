8ab88707ffcba8c6eb0e3834623bf43e54d41c82
Metin Ata YETER <ata@macbook.local>
2018-03-06 18:40:22 +0300

    cluster support

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
+import cluster from 'cluster'
+import os from 'os'
+
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
@@ -7,34 +10,55 @@ import routes from './routes'
 import mongoose from 'mongoose'
 import data from './data'
 
+const cpus = os.cpus().length;
+
 mongoose.connect(`mongodb://${config.mongo.host}:27017/GetirBiTaksiHackathon`);
 
 mongoose.connection.on('connected', () => {
-    mongoose.connection.db ? mongoose.connection.db.dropDatabase() : null;
+    //mongoose.connection.db ? mongoose.connection.db.dropDatabase() : null;
     data.seed();
 });
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/', routes);
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+    // Fork workers.
+    for (let i = 0; i < cpus; i++) {
+        cluster.fork();
+    }
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+}
+else {
+    const app = express();
+
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use('/', routes);
+
+    // catch 404 and forward to error handler
+    app.use((req, res, next) => {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    // error handler
+    app.use((err, req, res, next) => {
+        // render the error page
+        res.status(err.status || 500);
+        res.json(err);
+    });
+
+    app.listen(config.expressPort, () => {
+        console.log(`API is running on ${config.expressPort} port`);
+    });
+}
+
+cluster.on('exit', function (worker) {
+    // Replace the dead worker,
+    // we're not sentimental
+    console.log('Worker %d died :(', worker.id);
+    cluster.fork();
 });
-
-// error handler
-app.use((err, req, res, next) => {
-    // render the error page
-    res.status(err.status || 500);
-    res.json(err);
-});
-
-app.listen(config.expressPort, () => {
-    console.log(`API is running on ${config.expressPort} port`);
-});
-
-export default app
